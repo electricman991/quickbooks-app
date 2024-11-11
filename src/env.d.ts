30cf3b98c918466db00c3c62cda9bd2e74324068
electricman991 <braidensommer@yahoo.com>
2024-11-11 00:28:40 -0800

    Added tailwind and shadcn components for styling

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+    readonly QUICKBOOKS_CLIENT_ID: string;
+    readonly QUICKBOOKS_CLIENT_SECRET: string;
+    readonly QUICKBOOKS_REDIRECT_URI: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
